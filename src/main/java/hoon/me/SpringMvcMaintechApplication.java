f45ff40bba25be403a82f8acbeea8068e7d092c5
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-12 23:31:50 +0900

    Initial commit

---
+++ src/main/java/hoon/me/SpringMvcMaintechApplication.java
@@ -0,0 +1,13 @@
+package hoon.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMaintechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMaintechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hoon/me/SpringMvcMaintechApplicationTests.java
@@ -0,0 +1,13 @@
+package hoon.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMaintechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
