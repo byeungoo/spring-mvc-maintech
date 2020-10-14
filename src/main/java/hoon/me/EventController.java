57848a8c68891612dcba4f2bc560f7f4ead64ccd
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-14 23:48:17 +0900

    스프링 MVC 기초 예제 작성

--- src/main/java/hoon/me/EventController.java
+++ src/main/java/hoon/me/EventController.java
@@ -1,15 +1,19 @@
 package hoon.me;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class EventController {
 
-    @RequestMapping(value = "/events", method = RequestMethod.GET)
+    private final EventService eventService;
+
+    @GetMapping(value = "/events")
     public String events(Model model){
+        model.addAttribute("events", eventService.getEvents());
         return "events";
     }
 
